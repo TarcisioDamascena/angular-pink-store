459d36ace7d83fc2598f8d5ed67d2fbdea1c416e
DamasX <84998412+TarcisioDamascena@users.noreply.github.com>
2024-11-07 22:07:57 -0300

    Update: Menu bar and global styles

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,7 +1,7 @@
 export interface Product {
   id?: number;
-  name: number;
-  description: number;
+  name: string;
+  description: string;
   price: number;
-  imageUrl: number;
+  imageUrl: string;
 }
