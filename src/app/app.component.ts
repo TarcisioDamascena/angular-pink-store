691288bbb66a23977e50de2ef81ef58536d3a285
DamasX <84998412+TarcisioDamascena@users.noreply.github.com>
2024-11-12 16:24:53 -0300

    Feat: connectio with api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 import { MenuBarComponent } from "./components/menu-bar/menu-bar.component";
 
 @Component({
--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Product } from '../../models/product';
+import { ProductService } from '../../services/product.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], // Importa o HttpClientModule
   templateUrl: './products-list.component.html',
-  styleUrl: './products-list.component.css'
+  styleUrls: ['./products-list.component.css'],
 })
-export class ProductsListComponent {
+export class ProductsListComponent implements OnInit {
+  products: Product[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  loadProducts(): void {
+    this.productService.getProducts().subscribe({
+      next: (data) => (this.products = data),
+      error: (error) => console.error('Error fetching products', error),
+    });
+  }
 }
